5c8fbf9a62a266e34b05fad081f7b86dd2f2df2d
muhammadaliawan <ali.awan@square63.org>
2020-01-09 16:38:20 +0500

    Added roles and other attributes in User model

---
+++ db/migrate/20200109075337_add_fields_to_user.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :address, :string
+    add_column :users, :cnic, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :role, :integer
+  end
+end
