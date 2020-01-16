9477ab5be07bfb6d1be7fc814262e212efa73f8c
muhammadaliawan <ali.awan@square63.org>
2020-01-16 14:21:34 +0500

    Removed alert message from home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ActionController::Base
   include ApplicationHelper
 
   def index
-    redirect_to users_home_path, notice: 'You are already signed in' if user_signed_in?
+    redirect_to users_home_path if user_signed_in?
   end
 end
