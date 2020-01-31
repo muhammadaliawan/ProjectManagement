acd0a908ea1a5226e80bcb919a302b13a878e113
muhammadaliawan <ali.awan@square63.org>
2020-01-31 18:21:29 +0500

    Projects: Added chosen rails

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,55 +1,90 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i[show edit update destroy]
+  include ProjectHelper
+
+  before_action :set_project, only: %i[get_developers show edit update destroy]
+  before_action :get_client, :get_manager, :get_developer, only: %i[new edit]
 
   def index
-    @projects = Project.all
+    @projects = Project.get_projects(current_user).page(params[:page])
   end
 
   def new
     @project = Project.new
-    @clients = Client.order(:name)
+    authorize @project
   end
 
-  def show; end
+  def show
+    authorize @project
+    @payments = @project.payments.page(params[:payment_page])
+    @time_logs = @project.time_logs.page(params[:time_log_page])
+    @attachments = @project.attachments.page(params[:attachment_page])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 
   def edit
-    @clients = Client.order(:name)
+    authorize @project
   end
 
   def create
-    @client = Client.find(params[:project][:client])
     @project = Project.new(project_params)
-    @project.client = @client
+    authorize @project
 
-    if @project.save
-      redirect_to manager_project_path(@project), notice: 'Project created successfully.'
+    if @project.save!
+      redirect_to path_method(:show, :project, @project), notice: 'Project created successfully.'
     else
       render :new
     end
   end
 
   def update
+    authorize @project
+
+    @client = Client.find(params[:project][:client])
+    @manager = User.find(params[:project][:manager])
+    @user = User.find(params[:project][:resources])
+    @project.client = @client
+    @project.manager = @manager
+    @project.resources << @user
+
     if @project.update(project_params)
-      redirect_to project_path(@project), notice: 'Project successfully updated.'
+      redirect_to path_method(:show, :project, @project), notice: 'Project successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
+    authorize @project
+
     @project.destroy
-    redirect_to projects_url, notice: 'Project successfully deleted.'
+    redirect_to path_method(:index, :project, @projects), notice: 'Project successfully deleted.'
   end
 
   private
 
+  def get_developer
+    @developers = User.developers
+  end
+
+  def get_manager
+    @manager = User.managers.order(:name)
+  end
+
+  def get_client
+    @clients = Client.order(:name)
+  end
+
   def set_project
-    @project = Project.find(params[:id])
+    @project = Project.includes(:payments, :time_logs, :comments).find(params[:id])
   end
 
   def project_params
-    params.require(:project).permit(:name, :details, :total_paymentss, :time_log)
+    params.require(:project).permit(:name, :details, :client_id, :manager_id, :created_by_id, resource_ids: [])
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@
 class Project < ApplicationRecord
   paginates_per 5
 
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :resources, class_name: 'User'
 
   has_many :time_logs
   has_many :payments
@@ -11,6 +11,20 @@ class Project < ApplicationRecord
   has_many :attachments, as: :attachable
 
   belongs_to :client
+  belongs_to :manager, class_name: 'User'
+  belongs_to :created_by, class_name: 'User'
 
   validates :name, :details, presence: true
+
+  private
+
+  def self.get_projects(user)
+    if user.admin?
+      @project = Project.all
+    elsif user.manager?
+      @project = Project.where(manager: user).or(Project.where(created_by: user))
+    elsif user.developer?
+      @project = user.projects
+    end
+  end
 end
