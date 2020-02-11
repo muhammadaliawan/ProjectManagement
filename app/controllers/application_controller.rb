9815798492b8f9100eea7a9558bdede811477d5a
muhammadaliawan <ali.awan@square63.org>
2020-02-12 00:36:14 +0500

    Application Controller: Added record not found exception in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -18,6 +19,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def record_not_found
+    flash[:alert] = 'The specified record does not exist'
+    redirect_to request.referrer || root_path
+  end
 
   def user_not_authorized
     flash[:alert] = 'Access denied.'
